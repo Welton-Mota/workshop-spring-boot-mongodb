b82d48d7526282564f22431c08036587d295fc32
Welton Mota <welton.g.motta@gmail.com>
2022-06-08 17:29:28 -0300

    Creating project

---
+++ src/main/java/com/wmsolucoes/workshopmongo/WorkshopMongoApplication.java
@@ -0,0 +1,13 @@
+package com.wmsolucoes.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopMongoApplication.class, args);
+	}
+
+}
