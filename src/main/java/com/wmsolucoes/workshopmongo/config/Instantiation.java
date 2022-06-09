e193900038b01733b76369a1a582d205ad5af58c
Welton Mota <welton.g.motta@gmail.com>
2022-06-09 15:35:43 -0300

    Add comments

--- src/main/java/com/wmsolucoes/workshopmongo/config/Instantiation.java
+++ src/main/java/com/wmsolucoes/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.wmsolucoes.workshopmongo.domain.Post;
 import com.wmsolucoes.workshopmongo.domain.User;
 import com.wmsolucoes.workshopmongo.dto.AuthorDTO;
+import com.wmsolucoes.workshopmongo.dto.CommentDTO;
 import com.wmsolucoes.workshopmongo.repository.PostRepository;
 import com.wmsolucoes.workshopmongo.repository.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou Viajar para São Paulo. Abraços.", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
