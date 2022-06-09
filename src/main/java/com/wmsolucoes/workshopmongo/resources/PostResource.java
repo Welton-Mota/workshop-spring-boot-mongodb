a1f73a56b5affcef5694aa1b309e67a574ab7246
Welton Mota <welton.g.motta@gmail.com>
2022-06-09 15:58:28 -0300

    Simple cons with query methods

--- src/main/java/com/wmsolucoes/workshopmongo/resources/PostResource.java
+++ src/main/java/com/wmsolucoes/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.wmsolucoes.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wmsolucoes.workshopmongo.domain.Post;
+import com.wmsolucoes.workshopmongo.resources.util.URL;
 import com.wmsolucoes.workshopmongo.services.PostService;
 
 // para indicar que a classe será um recurso Rest
@@ -25,4 +29,10 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);
 	}
 
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
 }
