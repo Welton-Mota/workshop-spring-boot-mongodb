56a598dba604b44df152b6263b2807b02698578f
Welton Mota <welton.g.motta@gmail.com>
2022-06-09 10:42:52 -0300

    Obtain User by id

--- src/main/java/com/wmsolucoes/workshopmongo/services/UserService.java
+++ src/main/java/com/wmsolucoes/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.wmsolucoes.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wmsolucoes.workshopmongo.domain.User;
 import com.wmsolucoes.workshopmongo.repository.UserRepository;
+import com.wmsolucoes.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found."));
 	}
 }
