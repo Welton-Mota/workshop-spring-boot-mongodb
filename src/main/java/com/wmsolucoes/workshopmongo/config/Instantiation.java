165a1c4b69b1896f33e9d49678b22c5d198f28c0
Welton Mota <welton.g.motta@gmail.com>
2022-06-09 10:06:58 -0300

    Instantiation database

---
+++ src/main/java/com/wmsolucoes/workshopmongo/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.wmsolucoes.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.wmsolucoes.workshopmongo.domain.User;
+import com.wmsolucoes.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+	}
+}
